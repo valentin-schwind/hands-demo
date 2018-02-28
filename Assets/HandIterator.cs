4a7a9835c2f6522a28dd8fed458f2116d10ecc1f
valentin-schwind <mail@vali.de>
2018-02-28 17:19:55 +0100

    Demo fürs Schiff

---
+++ Assets/HandIterator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandIterator : MonoBehaviour
+{
+
+    public List<GameObject> handList = new List<GameObject>();
+    public int currentHandindex = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        showHands();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            nextHand();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            previousHand();
+        }
+    }
+
+    private void previousHand()
+    {
+        Debug.Log("Previous Hand");
+        currentHandindex--;
+        if (currentHandindex < 0)
+            currentHandindex = handList.Count - 1;
+        showHands();
+    }
+
+    private void showHands()
+    {
+        Debug.Log("Show Hand");
+        int i = 0;
+        foreach (GameObject child in handList)
+        {
+            child.SetActive(true);
+            if (i != currentHandindex) child.SetActive(false);
+            i++;
+        }
+    }
+   
+    private void nextHand()
+    {
+        Debug.Log("Previous Hand");
+        currentHandindex++;
+        if (currentHandindex > handList.Count - 1)
+            currentHandindex = 0;
+        showHands();
+    }
+}
---
+++ Assets/KeyboardShit.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardShit : MonoBehaviour {
+
+    public TextMesh textMesh;
+    public GameObject scene;
+    public Camera cameraRig;
+
+    private float sceneHeight = 0;
+	// Use this for initialization
+	void Start () {
+        textMesh.text = "";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(key))
+            {
+                switch (key.ToString().ToLower())
+                {
+                    case "r":
+                        scene.transform.position.Set(cameraRig.transform.position.x, sceneHeight, cameraRig.transform.position.z) ;
+                        break;
+                    case "space":
+                        textMesh.text += " ";
+                        break;
+                    case "return":
+                        textMesh.text += "\n";
+                        break;
+                    case "mouse0": break;
+                    case "mouse1": break;
+                    case "mouse2": break;
+                    case "leftalt": break;
+                    case "rightalt": break;
+                    case "leftarrow": break;
+                    case "rightarrow": break;
+                    case "uparrow":
+                        
+                        break;
+                    case "downarrow":
+                        
+                        break;
+                    case "left": break;
+                    case "right": break;
+                    case "alt": break;
+                    case "leftshift": break;
+                    case "rightshift": break;
+                    case "leftcontro": break;
+                    case "rightcontrol": break;
+                    case "leftcommand": break;
+                    case "rightcommand": break;
+                    default:
+                        textMesh.text += key.ToString().ToLower();
+                        break;
+                }
+                
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            textMesh.text = "";
+        }
+    }
+}
